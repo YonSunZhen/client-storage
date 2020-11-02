966d479d916bc7e511dca5382be877566d77a32f
YonSunZhen <1315976441@qq.com>
2020-11-02 15:21:34 +0800

    fix img css

--- src/app/@admin/api.url.ts
+++ src/app/@admin/api.url.ts
@@ -7,7 +7,7 @@ HOST_URL = 'http://localhost:8080';
 
 if (environment.production) {
   // 正式版
-  // HOST_URL = 'http://122.51.184.238/storage/api/v1';
+  HOST_URL = 'http://122.51.184.238/storage/api/v1';
 }
 export { HOST_URL };
 
